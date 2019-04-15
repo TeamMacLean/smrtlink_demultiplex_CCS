6c2be8f6fb06d8d5f3dfbb2e056a9b83314e1a04
tsl-ramkrishna <ram_krishna.shrestha@tsl.ac.uk>
2019-04-15 17:15:48 +0100

    script updated

--- scripts/run_pbsmrtpipe.sh
+++ scripts/run_pbsmrtpipe.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source smrtlink-6.0
-srun dataset create --type SubreadSet --force --name sebastian --generateIndices sequeldata.subreadset.xml input.fofn
-srun dataset create --type BarcodeSet --force --name barcodes --generateIndices singleBarcoding.barcodeset.xml barcodes8bps.fasta
-srun pbsmrtpipe pipeline-id pbsmrtpipe.pipelines.sa3_ds_barcode_ccs -o CCS_demultiplex  -e eid_subread:sequeldata.subreadset.xml -e eid_barcode:singleBarcoding.barcodeset.xml --preset-xml ./hpc_preset.xml --preset-xml ./ccs_preset.xml
+srun dataset create --force --type SubreadSet --name sebastian --generateIndices sequeldata.subreadset.xml input.fofn
+srun dataset create --force --type BarcodeSet --name barcodes --generateIndices singleBarcoding.barcodeset.xml barcodes8bps.fasta
+srun pbsmrtpipe pipeline-id -o CCS_demultiplex  -e "eid_subread:sequeldata.subreadset.xml" -e "eid_barcode:singleBarcoding.barcodeset.xml" --preset-xml ./hpc_preset.xml --preset-xml ./ccs_preset.xml --debug pbsmrtpipe.pipelines.sa3_ds_ccs_barcode
